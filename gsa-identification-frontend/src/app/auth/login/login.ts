0a8088b9eb7190044b4b239cfe29ebfbecc42484
Tharushi111 <gaminiparanagama2@gmail.com>
2026-09-25 09:16:05 +0530

    Implement applicant login page and authentication routes

--- gsa-identification-frontend/src/app/auth/login/login.ts
+++ gsa-identification-frontend/src/app/auth/login/login.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
   selector: 'app-login',
-  styleUrl: './login.css',
+  imports: [
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './login.html',
+  styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+
+  loginForm: FormGroup;
+  showPassword = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      password: [
+        '',
+        Validators.required
+      ]
+    });
+  }
+
+  togglePassword(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(): void {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log('Login data:', this.loginForm.value);
+
+    // Backend API connection will be added later.
+  }
+}
