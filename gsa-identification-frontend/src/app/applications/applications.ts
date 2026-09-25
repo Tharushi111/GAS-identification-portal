cc60195623447213fcff9339ff38ebfe20cc24c3
Tharushi111 <gaminiparanagama2@gmail.com>
2026-09-25 15:59:12 +0530

    Implement applicant applications page

--- gsa-identification-frontend/src/app/applications/applications.ts
+++ gsa-identification-frontend/src/app/applications/applications.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
 
+import { Header } from '../shared/header/header';
+import { AccountSidebar } from '../shared/account-sidebar/account-sidebar';
+import { Footer } from '../shared/footer/footer';
+
 @Component({
-  imports: [],
   selector: 'app-applications',
-  styleUrl: './applications.css',
+  imports: [
+    Header,
+    AccountSidebar,
+    Footer
+  ],
   templateUrl: './applications.html',
+  styleUrl: './applications.css'
 })
-export class Applications {}
+export class Applications {
+
+  application = {
+    applicationId: 'GSA-2026-000184',
+    applicationType: 'Passenger',
+    territory: 'United Kingdom',
+    submittedDate: '09/08/2026',
+    status: 'Submitted'
+  };
+
+  viewApplication(): void {
+    console.log(
+      'View application:',
+      this.application.applicationId
+    );
+
+    // Later we will navigate to the Application Details page.
+  }
+}
